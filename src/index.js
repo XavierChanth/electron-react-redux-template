78affd2c0c57380e0146f9256ef6e4bae2ebdcab
XavierChanth <xchanthavong@gmail.com>
2020-02-01 17:15:32 -0500

    basic redux setup

--- src/index.js
+++ src/index.js
@@ -3,4 +3,14 @@ import ReactDOM from 'react-dom';
 
 import App from './app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
+
+
+let store = createStore(reducers);
+store.subscribe(() => console.log('SUBSCRIBED TO STORE'));
+
+ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
+
+
