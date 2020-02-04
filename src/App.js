ea9a5166a89f1ecc863f0493dac19cb3616c00dd
XavierChanth <xchanthavong@gmail.com>
2020-02-04 13:03:19 -0500

    add counter example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {increment, decrement} from './actions'
 
 const App = () => {
-  return <h1>Hello World!</h1>
+  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch();
+
+  return <div>
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+    </div>
 };
 
 export default App;
