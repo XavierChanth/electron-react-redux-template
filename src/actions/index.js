f452b5dfa0eceb05568897714622716a3ea87306
XavierChanth <xchanthavong@gmail.com>
2020-02-04 13:04:10 -0500

    add actions file

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+//counter
+export const increment = (num) => {
+  return {
+    type: 'INCREMENT',
+    payload: num
+  }
+}
+
+export const decrement = (num) => {
+  return {
+    type: 'DECREMENT',
+    payload: num
+  }
+}
+
