85ab47f9cb1ba5498e3c103bb8b66b274d389038
XavierChanth <xchanthavong@gmail.com>
2020-02-04 13:04:21 -0500

    add dynamic payload to counter

--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -1,9 +1,9 @@
 export default (state = 0, action) => {
   switch (action.type) {
     case 'INCREMENT':
-      return state + 1;
+      return state + (action.payload || 1);
     case 'DECREMENT':
-      return state - 1;
+      return state - (action.payload || 1);
     default:
       return state;
   }
