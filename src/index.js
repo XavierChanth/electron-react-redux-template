fc355f902027a603f672c321161c6ef7d3666c78
XavierChanth <xchanthavong@gmail.com>
2020-02-04 16:08:37 -0500

    load and save the state

--- src/index.js
+++ src/index.js
@@ -5,13 +5,20 @@ import App from './app';
 
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+
 import reducers from './reducers';
+import {loadState, saveState} from './localStorage'
 
+const persistedState = loadState();
+const store = createStore(
+  reducers,
+  persistedState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 
-let store = createStore(reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
-store.subscribe(() => console.log('SUBSCRIBED TO STORE'));
+store.subscribe(() => {
+  saveState(store.getState())
+});
 
 ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
 
